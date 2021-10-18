4aaba73bd89ca3e34329df031a178e009f2a753b
杜宏磊 <1931521946@qq.com>
2021-10-18 15:15:56 +0800

    提交Vue中代理服务器配置方法的学习代码

--- 02-vue-test/vue.config.js
+++ 02-vue-test/vue.config.js
@@ -1,3 +1,4 @@
+const {devServer} = require("./vue.config");
 module.exports = {
   pages: {
     index: {
@@ -5,5 +6,27 @@ module.exports = {
       entry: 'src/main.js'
     }  
   },
-  lintOnSave: false
+  // 关于语法检查
+  lintOnSave: false,
+  // 开启代理服务器(方式一)
+  /*devServer: {
+    proxy: 'http://localhost:5000'
+  },*/
+  // 开启代理服务器(方式二)
+  devServer: {
+    proxy: {
+      '/wenze': {
+        target: 'http://localhost:5000',
+        pathRewrite: {'^/wenze': ''},
+        // 用于支持WebSocket
+        // ws: true,
+        // 用于控制请求头中的host值
+        // changeOrigin: true
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: {'^/demo': ''},
+      }
+    }
+  }
 }
