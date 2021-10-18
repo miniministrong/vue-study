e0b848e36db5c16781cb2ddbdef30031ee6dd7fe
杜宏磊 <1931521946@qq.com>
2021-10-18 16:19:10 +0800

    提交Vue中github搜索练习的学习代码

---
+++ 02-vue-test/src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+          type="text"
+          placeholder="enter the name you search"
+          v-model="keyWord"
+      />&nbsp;
+      <button @click="getUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  import axios from "axios";
+  export default {
+    name: "Search",
+    data(){
+      return{
+        keyWord: ''
+      }
+    },
+    methods: {
+      getUsers(){
+        // 请求前更新List中的数据
+        this.$bus.$emit('updateListData', {isFirst: false, isLoading: true, errMsg: '', users: []});
+        axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+            response => {
+              console.log("请求成功了！");
+              // 请求成功后更新List中的数据
+              this.$bus.$emit('updateListData', {
+                    isLoading: false,
+                    errMsg: '',
+                    users: response.data.items
+                  });
+            },
+            error => {
+              // 请求失败后更新List的数据
+              this.$bus.$emit('updateListData', {
+                isLoading: false,
+                errMsg: error.message,
+                users: []
+              });
+            }
+        );
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
