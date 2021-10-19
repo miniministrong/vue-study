57b245afc19cec63f608aa3437e48a1e0880a94a
杜宏磊 <1931521946@qq.com>
2021-10-19 18:54:28 +0800

    提交使用Vuex版求和案例的学习代码

---
+++ 02-vue-test/src/store/index.js
@@ -0,0 +1,46 @@
+// 该文件用于创建Vuex中最为核心的store
+
+// 引入Vue
+import Vue from 'vue'
+// 引入Vuex
+import Vuex from 'vuex'
+// 使用Vuex插件
+Vue.use(Vuex)
+
+// 准备actions——用于响应组件中的动作
+const actions = {
+  /*add(context, value){
+    context.commit("ADD", value)
+  },
+  sub(context, value){
+    context.commit("SUB", value)
+  },*/
+  odd(context, value){
+    if (context.state.sum % 2) {
+      context.commit("ADD", value);
+    }
+  },
+  wait(context, value){
+    setTimeout(() => context.commit("ADD", value), 500);
+  }
+}
+// 准备mutations——用于操作数据（state）
+const mutations = {
+  ADD(state, value){
+    state.sum += value
+  },
+  SUB(state, value) {
+    state.sum -= value;
+  }
+}
+// 准备state——用于存储数据
+const state = {
+  sum:0
+}
+
+// 创建并暴露store
+export  default new Vuex.Store({
+  actions,
+  mutations,
+  state,
+})
