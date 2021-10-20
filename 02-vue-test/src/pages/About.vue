ebab3e11e60e0f3ef0ed4b833bafd76f08346a96
杜宏磊 <1931521946@qq.com>
2021-10-20 10:48:23 +0800

    提交Vue中多级路由使用部分的学习代码

---
+++ 02-vue-test/src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>我是About的内容</h2>
+</template>
+
+<script>
+  export default {
+    name: "About",
+    /*beforeDestroy() {
+      console.log("About组件即将被销毁")
+    },
+    mounted() {
+      console.log("About组件被挂载完毕了")
+    }*/
+  }
+</script>
+
+<style scoped>
+
+</style>
